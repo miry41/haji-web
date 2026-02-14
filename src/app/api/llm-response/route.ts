ce1756985b11f1e2cb557dd018190ff2716c2f7b
takami <takami.ku.518@gmail.com>
2026-02-15 03:27:06 +0900

    Gemini API叩いてレスポンス貰うまで

--- src/app/api/llm-response/route.ts
+++ src/app/api/llm-response/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { SYSTEM_PROMPT } from "./prompt";
 
 type LlmResponseRequestBody = {
   message?: string;
@@ -9,6 +10,32 @@ export async function POST(req: Request) {
   const message = body.message ?? "";
 
   console.log(message);
+
+  // ★プロンプトと入力値の結合
+  const promptWithMessage = `${SYSTEM_PROMPT}${message}`;
+  console.log(promptWithMessage);
+
+  // ★LLM API呼び出し
+  const apiKey = process.env.GEMINI_API_KEY;
+  const response = await fetch(
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        contents: [
+          {
+            parts: [{ text: promptWithMessage }],
+          },
+        ],
+      }),
+    }
+  );
+  const data = await response.json();
+  console.log(data?.candidates?.[0]?.content?.parts?.[0]?.text ?? "");
+  
 
   return NextResponse.json({ ok: true });
 }
